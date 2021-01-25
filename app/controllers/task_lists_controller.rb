0f17231404b068412e095995ad3911ec4e7d66fb
RhinoLee <picklerhino@gmail.com>
2021-01-25 18:37:25 +0800

    task routes setting

--- app/controllers/task_lists_controller.rb
+++
@@ -1,8 +0,0 @@
-class TaskListsController < ApplicationController
-
-
-  def create
-    @task_list = TaskList.new
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :task, only: [:create, :update, :destroy]
+  resources :tasks
 
-  resources :task_lists, only: [:index, :create, :update, :destroy]
-
-  root 'task_lists#index'
+  root 'tasks#index'
 
 end
