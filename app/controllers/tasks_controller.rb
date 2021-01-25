2f2db92381de28f84ce9333fc8ff8de347e8d9d6
RhinoLee <picklerhino@gmail.com>
2021-01-26 01:48:33 +0800

    task Read Update Destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,30 @@ class TasksController < ApplicationController
 
   end
 
+  def show 
+    @task = Task.find(params[:id])
+  end
+
+  def edit 
+    @task = Task.find(params[:id])
+  end
+
+  def update 
+    @task = Task.find(params[:id]) 
+
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: "任務更新成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    @task = Task.find(params[:id]) 
+    @task.destroy
+    redirect_to :root, notice: "任務已刪除"
+  end
+
 
   private 
   def task_params 
