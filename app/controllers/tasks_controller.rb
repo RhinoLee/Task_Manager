bc88c0815ff85a3c238ca82c1df383c2ab50810c
RhinoLee <picklerhino@gmail.com>
2021-02-03 10:04:29 +0800

    add task status option

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
 
   private 
   def task_params 
-    params.require(:task).permit(:title, :content, :end_time)
+    params.require(:task).permit(:title, :content, :end_time, :status)
   end
 
   def find_task 
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,6 @@ class Task < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true
-  # validates :title, presence: { message: "任務名稱不得空白" }
-  # validates :content, presence: { message: "任務內容不得空白" }
+  validates :status, presence: true
 
 end
