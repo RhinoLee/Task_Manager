e41ebe0d7ed2331edcda28ec11dfcbc068e1bfab
RhinoLee <picklerhino@gmail.com>
2021-02-20 11:43:05 +0800

    add feature for level sort

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -33,4 +33,28 @@ RSpec.feature "Task management", :type => :feature do
 
   end
 
+  scenario "任務依照優先順序由高到低排序" do
+    visit "/"
+
+    task_important = Task.create(title: "task_important", content: "old content", end_time: "2021/02/17", status: "待處理", level: 2)
+    task_lazy = Task.create(title: "task_lazy", content: "new content", end_time: "2021/04/20", status: "待處理", level: 0)
+
+    find('#level_desc_btn').click
+
+    expect( page.body.index(task_important.title) ).to be < page.body.index(task_lazy.title)
+
+  end
+
+  scenario "任務依照優先順序由低到高排序" do
+    visit "/"
+
+    task_important = Task.create(title: "task_important", content: "old content", end_time: "2021/02/17", status: "待處理", level: 2)
+    task_lazy = Task.create(title: "task_lazy", content: "new content", end_time: "2021/04/20", status: "待處理", level: 0)
+
+    find('#level_asc_btn').click
+
+    expect( page.body.index(task_important.title) ).to be > page.body.index(task_lazy.title)
+
+  end
+
 end
