b8df54b0c523e7e68c4d6b604be48a0f58af888b
RhinoLee <picklerhino@gmail.com>
2021-02-25 18:37:56 +0800

    add user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,25 @@
 class SessionsController < ApplicationController
 
+  def new 
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+
+      redirect_to :root, notice: "登入成功"
+    else 
+      flash[:alert] = '信箱或密碼輸入錯誤'
+      render :new
+    end
+
+  end
+
   def destroy 
     session[:user_id] = nil
     redirect_to :root, notice: '已登出'
-
   end
 
 end
