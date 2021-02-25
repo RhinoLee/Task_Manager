0d7ece81be37a3d76badb02bb66fade843e1195b
RhinoLee <picklerhino@gmail.com>
2021-02-26 01:58:27 +0800

    add current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
   unless Rails.env.test? 
     http_basic_authenticate_with :name => ENV["HTTP_BASIC_USER"], 
                                  :password => ENV["HTTP_BASIC_PASSWORD"] 
+  end
+
+  def set_current_user
+    if session[:id] 
+      Current.user = User.find_by(id: session[:id])
+      # current_user = User.find_by(id: session[:id])
+    end
   end
 
 end
